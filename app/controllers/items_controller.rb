256ec525fe9a57a42843c86a028d16a698119802
remclay <remmy.lauren@gmail.com>
2018-04-09 21:08:31 -0600

    Resolve conflict in ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,6 @@
 class ItemsController < ApplicationController
-<<<<<<< HEAD
 
   def show
     @item = Item.find_by(id: params[:id])
-=======
-  def show
-    @item = Item.find(params[:id])
->>>>>>> 75e99cc36bf9088efca1f5e2c6ae583ce3146a26
   end
 end
